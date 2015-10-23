69f7d8ab28023378432581e248e9ad8e28433296
孤月蓝风 <trlanfeng@foxmail.com>
2015-10-24 00:33:35 +0800

    整理代码存放位置，将存档功能独立为组件

---
+++ Assets/_Scripts/Class/Hero.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Hero : ActItem
+{
+    public string name;
+    public string png;
+    public int level;
+    public int exp;
+    public int HPMax;
+    public int MPMax;
+    public int HP;
+    public int MP;
+    public int str;
+    public int mag;
+    public int def;
+    public int baseHP;
+    public int baseMP;
+    public int baseStr;
+    public int baseMag;
+    public int baseDef;
+    public int weaponID;
+    public int armorID;
+
+    public Hero()
+    {
+        base.ActorType = 1;
+        level = 1;
+        exp = 0;
+
+        HPMax = 12;
+        MPMax = 0;
+        HP = HPMax;
+        MP = MPMax;
+        str = 5;
+        mag = 0;
+        def = 2;
+
+        baseHP = 1;
+        baseMP = 1;
+        baseStr = 1;
+        baseMag = 1;
+        baseDef = 1;
+
+        LoadData();
+
+        checkLevelUp();
+    }
+
+    public void setJsonToHero(JSONObject jo)
+    {
+        jo.GetField(ref baseHP, "baseHP");
+        jo.GetField(ref baseMP, "baseMP");
+        jo.GetField(ref baseStr, "baseStr");
+        jo.GetField(ref baseMag, "baseMag");
+        jo.GetField(ref baseDef, "baseDef");
+        checkLevelUp();
+    }
+
+    public void checkLevelUp()
+    {
+        int _constExp = Mathf.RoundToInt(Mathf.Pow((level), 0.4f) * Mathf.Pow(level, 2) * 5);
+        while (exp >= _constExp)
+        {
+            level += 1;
+            levelUp();
+            exp -= _constExp;
+            _constExp = Mathf.RoundToInt(Mathf.Pow((level), 0.4f) * Mathf.Pow(level, 2) * 5);
+        }
+    }
+
+    public void levelUp()
+    {
+        HPMax = Mathf.RoundToInt(Mathf.Pow(baseHP, 0.4f) * HPMax);
+        MPMax = Mathf.RoundToInt(Mathf.Pow(baseMP, 0.4f) * MPMax);
+        HP = HPMax;
+        MP = MPMax;
+        str = str + baseStr;
+        mag = mag + baseMag;
+        def = def + baseDef;
+    }
+
+    public void SaveData()
+    {
+        JSONObject heroJsonData = new JSONObject(JSONObject.Type.OBJECT);
+        heroJsonData.AddField("level", level);
+        heroJsonData.AddField("exp", exp);
+        heroJsonData.AddField("HPMax", HPMax);
+        heroJsonData.AddField("MPMax", MPMax);
+        heroJsonData.AddField("str", str);
+        heroJsonData.AddField("mag", mag);
+        heroJsonData.AddField("def", def);
+        Debug.Log("heroData:" + heroJsonData.ToString());
+        PlayerPrefs.SetString("heroJsonData", heroJsonData.ToString());
+    }
+
+    public void LoadData()
+    {
+        if (PlayerPrefs.HasKey("heroJsonData"))
+        {
+            string heroJsonData = PlayerPrefs.GetString("heroJsonData");
+            JSONObject j = new JSONObject(heroJsonData);
+            j.GetField(ref level, "level");
+            j.GetField(ref exp, "exp");
+            j.GetField(ref HPMax, "HPMax");
+            j.GetField(ref MPMax, "MPMax");
+            j.GetField(ref str, "str");
+            j.GetField(ref mag, "mag");
+            j.GetField(ref def, "def");
+            HP = HPMax;
+            MP = MPMax;
+        }
+    }
+}
+
---
+++ Assets/_Scripts/Manager/GameDataManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameDataManager : MonoBehaviour
+{
+
+    private int itemCount = 5;
+
+    GameManager GM;
+
+    void Start()
+    {
+        GM = transform.GetComponent<GameManager>();
+    }
+    //根据文档名保存存档，自动存档为auto，其他为数字编号
+    public void SaveData(string dataName)
+    {
+        JSONObject gameData = new JSONObject(JSONObject.Type.OBJECT);
+        gameData.AddField("level", GM.level);
+        gameData.AddField("gold", GM.player.gold);
+        for (int i = 0; i < itemCount; i++)
+        {
+            gameData.AddField("item" + i + "Count", GM.player.itemCount[i]);
+        }
+        PlayerPrefs.SetString("gameData_" + dataName, gameData.ToString());
+    }
+    //根据文档名读取存档，自动存档为auto，其他为数字编号
+    public void LoadData(string dataName)
+    {
+        if (PlayerPrefs.HasKey("gameData_" + dataName))
+        {
+            string gameData = PlayerPrefs.GetString("gameData_" + dataName);
+            JSONObject j = new JSONObject(gameData);
+            j.GetField(ref GM.level, "level");
+            j.GetField(ref GM.player.gold, "gold");
+            for (int i = 0; i < 5; i++)
+            {
+                j.GetField(ref GM.player.itemCount[i], "item" + i + "Count");
+            }
+        }
+    }
+    //读取自动存档
+    public void LoadAutoData()
+    {
+        LoadData("auto");
+    }
+    //保存自动存档
+    public void SaveAutoData()
+    {
+        SaveData("auto");
+    }
+
+}
